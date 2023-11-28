9e8d854113b9c19a3e4e529db77751e174489c0a
Fares Hesham <66084853+fares-h-moneim@users.noreply.github.com>
2023-11-28 21:39:05 +0200

    28/11/2023

---
+++ Leet_Code/Medium/Container With Most Water.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int max = 0, i = 0, j = height.size()-1;
+        while(i<j){
+            int area = min(height[i], height[j])*(j-i);
+            if(area > max) max = area;
+
+            if(height[i]<=height[j]) i++;
+            else j--;
+        }
+        return max;
+    }
+};
