59fd48719329c9bfc22d5208910e62a1f89d062b
Fares Hesham <66084853+fares-h-moneim@users.noreply.github.com>
2023-12-01 11:00:51 +0200

    01/12/2023

--- Leet_Code/Medium/3Sum.cpp
+++ Leet_Code/Medium/3Sum.cpp
@@ -3,23 +3,21 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> ans;
         sort(nums.begin(), nums.end());
-
-        for (int i = 0; i < nums.size() - 2; ++i) {
-            int target = -nums[i];
-            int j = i + 1, k = nums.size() - 1;
-
-            while (j < k) {
-                int sum = nums[j] + nums[k];
-                if (sum == target) {
-                    ans.push_back({nums[i], nums[j], nums[k]});
+        for(int i = 0; i<nums.size(); i++){
+            int target = -1*nums[i];
+            int j = i+1, k = nums.size()-1;
+            vector<int> temp(3);
+            while(j<k){
+                if(nums[j]+nums[k]==target){
+                    temp[0] = nums[i]; temp[1] = nums[j]; temp[2] = nums[k];
+                    ans.push_back(temp);
                     while (j < k && nums[j] == nums[j + 1]) j++;
                     while (j < k && nums[k] == nums[k - 1]) k--;
-                    j++;
-                    k--;
-                } else if (sum < target) j++;
+                    j++; k--;
+                }
+                if(nums[j]+nums[k]<target) j++;
                 else k--;
             }
-            while (i < nums.size() - 2 && nums[i] == nums[i + 1]) i++;
         }
         return ans;
     }
