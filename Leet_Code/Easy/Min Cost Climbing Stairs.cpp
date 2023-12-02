26d9594c93f891f320978d250b44314bed117417
Fares Hesham <66084853+fares-h-moneim@users.noreply.github.com>
2023-12-02 15:39:53 +0200

    02/12/2023

--- Leet_Code/Easy/Min Cost Climbing Stairs.cpp
+++ Leet_Code/Easy/Min Cost Climbing Stairs.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
-    int minCostClimbingStairs(vector<int>& cost) {
-        vector<int> dp(cost.size(), -1);
-        return min(sol(cost, 0, dp), sol(cost, 1, dp));
-    }
-
-    int sol(vector<int>& cost, int n, vector<int>& dp) {
-        if (n >= cost.size()) return 0;
-        if (dp[n] != -1) return dp[n];
-        return dp[n] = cost[n] + min(sol(cost, n + 1, dp), sol(cost, n + 2, dp));
+    int minCostClimbingStairs(std::vector<int>& cost) {
+        int n = cost.size();
+        vector<int> dp(n + 1, INT_MAX);
+        dp[n] = 0; dp[n-1] = cost[n-1]; 
+        for (int i = n - 2; i >= 0; i--) dp[i] = cost[i] + min(dp[i+1], dp[i+2]);
+        return min(dp[0], dp[1]);
     }
 };
