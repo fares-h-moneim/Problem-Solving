190e3c463e4acd428efc94f30de6e445555e522f
Fares Hesham <66084853+fares-h-moneim@users.noreply.github.com>
2023-12-02 14:43:13 +0200

    02/12/2023

--- Leet_Code/Easy/Climbing Stairs.cpp
+++ Leet_Code/Easy/Climbing Stairs.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /* Top Down */
     int climbStairs(int n) {
         vector<int> dp(n+1, -1);
         return solution(dp, n);
@@ -9,5 +10,12 @@ public:
         if(n<0) return 0;
         if(dp[n] == -1) dp[n] = solution(dp, n-1) + solution(dp, n-2);
         return dp[n];
+    }
+
+    /* Bottom Up */
+    int climbStairs(int n) {
+        vector<int> dp(n+1, 1); dp[n] = 1; dp[n-1] = 1;
+        for(int i = n-2; i>=0; i--) dp[i] = dp[i+1] + dp[i+2];
+        return dp[0];
     }
 };
